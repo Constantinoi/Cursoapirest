0fbc22b29d5abd622cd379249a071ebae826e0f1
constantinoi <pescaalma@gmail.com>
2022-04-06 21:44:19 -0300

    Retornando usuário em JSON para a tela

--- src/main/java/com/api/rest/controller/IndexController.java
+++ src/main/java/com/api/rest/controller/IndexController.java
@@ -1,19 +1,23 @@
 package com.api.rest.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.rest.model.Usuario;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam(value = "nome" , defaultValue = "sem nome") String nome) {
-		System.out.println(nome);
-		return new ResponseEntity("Ola " + nome, HttpStatus.OK);
+	public ResponseEntity<Usuario> init() {
+		Usuario user = new Usuario();
+		user.setId(1L);
+		user.setLogin("fab");
+		user.setSenha("123");
+		user.setNome("Fabiano");
+		return ResponseEntity.ok(user);
 	}
 }
